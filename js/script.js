8dfade8c2357185cec809f6a320ad2590fc6acb5
AbhishekM989 <abhishekmaurya9f@gmail.com>
2022-06-06 14:39:34 +0530

    Added Work Section

--- js/script.js
+++ js/script.js
@@ -30,3 +30,24 @@ modalClose.forEach((mc) => {
         })
     })
 })
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/*=============== LINK ACTIVE WORK ===============*/
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+    
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
