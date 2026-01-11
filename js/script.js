3606be60a536b0bd234e0e62ca32fa36924cf628
AbhishekM989 <abhishekmaurya2510@gmail.com>
2026-01-11 21:39:00 +0530

    UNDER DEPLOYMENT

---
+++ js/script.js
@@ -0,0 +1,122 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== LEARNING MODAL ===============*/
+
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close');
+
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/*=============== LINK ACTIVE WORK ===============*/
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+
+}
+
+linkWork.forEach(l => l.addEventListener('click', activeWork))
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// const sections = document.querySelectorAll('section[id]')
+
+// function scrollActive(){
+//     const scrollY = window.scrollY
+
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//             sectionTop = current.offsetTop - 58,
+//             sectionId = current.getAttribute('id')
+
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
+
+/*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true,
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, {delay: 700})
+sr.reveal(`.home__social`, {delay: 900, origin: 'bottom'})
+
+/*=============== LOADING ANIMATION ===============*/
+var preloader = document.getElementById('loading');
+function myFunction(){
+    preloader.style.display = 'none';
+}
+
+const MAINTENANCE_MODE = true;
+
+  if (MAINTENANCE_MODE) {
+    window.location.replace("/maintenance.html");
+  }
